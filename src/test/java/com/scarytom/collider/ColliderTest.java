03cde182fd1fb47478f48543c36da945dd081c22
scarytom <t.denley@cantab.net>
2013-03-16 09:58:36 +0000

    model object as position and velocity

--- src/test/java/com/scarytom/collider/ColliderTest.java
+++ src/test/java/com/scarytom/collider/ColliderTest.java
@@ -9,24 +9,39 @@ public final class ColliderTest {
 
     @Test public void
     modelsSimpleMovement() {
-        double[] startPos = position(2.0, 4.0);
-        double[] velocity = new double[] {1.0, 0.5};
+        double[] objectAtStart = object(2.0, 4.0, 1.0, 0.5);
         
-        double[] positionAfterOneSecond = position(startPos[0] + velocity[0], startPos[1] + velocity[1]);
-        assertThat(positionAfterOneSecond, is(position(3.0, 4.5)));
+        double[] objectAfterOneSecond = object(objectAtStart[0] + objectAtStart[2],
+                                               objectAtStart[1] + objectAtStart[3],
+                                               objectAtStart[2],
+                                               objectAtStart[3]);
+        
+        assertThat(positionOf(objectAfterOneSecond), is(new double[]{3.0, 4.5}));
+        assertThat(velocityOf(objectAfterOneSecond), is(new double[]{1.0, 0.5}));
     }
 
     @Test public void
     modelsPositionDuringSimpleMovement() {
-        double[] startPos = position(2.0, 4.0);
-        double[] velocity = new double[] {1.0, 0.5};
+        double[] objectAtStart = object(2.0, 4.0, 1.0, 0.5);
         
-        double[] positionAfterAHundedthOfASecond = position(startPos[0] + velocity[0] / 100,
-                                                            startPos[1] + velocity[1] / 100);
-        assertThat(positionAfterAHundedthOfASecond, is(position(2.01, 4.005)));
+        double[] objectAfterOneSecond = object(objectAtStart[0] + objectAtStart[2] / 100,
+                objectAtStart[1] + objectAtStart[3] / 100,
+                objectAtStart[2],
+                objectAtStart[3]);
+        
+        assertThat(positionOf(objectAfterOneSecond), is(new double[]{2.01, 4.005}));
+        assertThat(velocityOf(objectAfterOneSecond), is(new double[]{1.0, 0.5}));
     }
-    
-    private static double[] position(double x, double y) {
-        return new double[] {x, y};
+
+    private static double[] object(double x, double y, double vx, double vy) {
+        return new double[] {x, y, vx, vy};
+    }
+
+    private static double[] positionOf(double[] object) {
+        return new double[]{object[0], object[1]};
+    }
+
+    private static double[] velocityOf(double[] object) {
+        return new double[]{object[2], object[3]};
     }
 }
