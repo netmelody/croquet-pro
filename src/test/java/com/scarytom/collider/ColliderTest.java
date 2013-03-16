fcf8c0deb236b193962450ac2973f6d570db3e6e
scarytom <t.denley@cantab.net>
2013-03-16 09:38:16 +0000

    extract position method

--- src/test/java/com/scarytom/collider/ColliderTest.java
+++ src/test/java/com/scarytom/collider/ColliderTest.java
@@ -9,10 +9,14 @@ public final class ColliderTest {
 
     @Test public void
     modelsSimpleMovement() {
-        double[] startPos = new double[] {2.0, 4.0};
+        double[] startPos = position(2.0, 4.0);
         double[] velocity = new double[] {1.0, 0.5};
         
-        double[] positionAfterOneSecond = new double[] {startPos[0] + velocity[0], startPos[1] + velocity[1]};
-        assertThat(positionAfterOneSecond, is(new double[] {3.0, 4.5}));
+        double[] positionAfterOneSecond = position(startPos[0] + velocity[0], startPos[1] + velocity[1]);
+        assertThat(positionAfterOneSecond, is(position(3.0, 4.5)));
+    }
+
+    private static double[] position(double x, double y) {
+        return new double[] {x, y};
     }
 }
