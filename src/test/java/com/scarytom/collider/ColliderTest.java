bbd8b5482d3f860c46856ffa01e3308f56b88009
scarytom <t.denley@cantab.net>
2013-03-16 09:45:23 +0000

    model simple movement after a fraction of a second

--- src/test/java/com/scarytom/collider/ColliderTest.java
+++ src/test/java/com/scarytom/collider/ColliderTest.java
@@ -16,6 +16,16 @@ public final class ColliderTest {
         assertThat(positionAfterOneSecond, is(position(3.0, 4.5)));
     }
 
+    @Test public void
+    modelsPositionDuringSimpleMovement() {
+        double[] startPos = position(2.0, 4.0);
+        double[] velocity = new double[] {1.0, 0.5};
+        
+        double[] positionAfterAHundedthOfASecond = position(startPos[0] + velocity[0] / 100,
+                                                            startPos[1] + velocity[1] / 100);
+        assertThat(positionAfterAHundedthOfASecond, is(position(2.01, 4.005)));
+    }
+    
     private static double[] position(double x, double y) {
         return new double[] {x, y};
     }
