554f90cfb3385d2f51912a2a7585002f3b32df33
scarytom <t.denley@cantab.net>
2013-03-16 10:14:32 +0000

    extract method for object position after n seconds

--- src/test/java/com/scarytom/collider/ColliderTest.java
+++ src/test/java/com/scarytom/collider/ColliderTest.java
@@ -16,10 +16,7 @@ public final class ColliderTest {
     modelsSimpleMovement() {
         double[] objectAtStart = object(2.0, 4.0, 1.0, 0.5);
         
-        double[] objectAfterOneSecond = object(objectAtStart[X] + objectAtStart[VX],
-                                               objectAtStart[Y] + objectAtStart[VY],
-                                               objectAtStart[VX],
-                                               objectAtStart[VY]);
+        double[] objectAfterOneSecond = positionAfter(objectAtStart, 1.0);
         
         assertThat(positionOf(objectAfterOneSecond), is(new double[]{3.0, 4.5}));
         assertThat(velocityOf(objectAfterOneSecond), is(new double[]{1.0, 0.5}));
@@ -29,17 +26,21 @@ public final class ColliderTest {
     modelsPositionDuringSimpleMovement() {
         double[] objectAtStart = object(2.0, 4.0, 1.0, 0.5);
         
-        double[] objectAfterOneSecond = object(objectAtStart[X] + objectAtStart[VX] / 100,
-                                               objectAtStart[Y] + objectAtStart[VY] / 100,
-                                               objectAtStart[VX],
-                                               objectAtStart[VY]);
+        double[] objectAfterOneHundredthOfASecond = positionAfter(objectAtStart, 0.01);
         
-        assertThat(positionOf(objectAfterOneSecond), is(new double[]{2.01, 4.005}));
-        assertThat(velocityOf(objectAfterOneSecond), is(new double[]{1.0, 0.5}));
+        assertThat(positionOf(objectAfterOneHundredthOfASecond), is(new double[]{2.01, 4.005}));
+        assertThat(velocityOf(objectAfterOneHundredthOfASecond), is(new double[]{1.0, 0.5}));
     }
 
     private static double[] object(double x, double y, double vx, double vy) {
         return new double[] {x, y, vx, vy};
+    }
+
+    private static double[] positionAfter(double[] objectAtStart, double time) {
+        return object(objectAtStart[X] + objectAtStart[VX] * time,
+                objectAtStart[Y] + objectAtStart[VY] * time,
+                objectAtStart[VX],
+                objectAtStart[VY]);
     }
 
     private static double[] positionOf(double[] object) {
