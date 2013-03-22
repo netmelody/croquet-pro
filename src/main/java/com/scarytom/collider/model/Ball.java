bfcf1a7a294bee05a149ab46aeff8a8d81db43a9
scarytom <t.denley@cantab.net>
2013-03-22 22:51:51 +0000

    make the black ball blacker

--- src/main/java/com/scarytom/collider/model/Ball.java
+++ src/main/java/com/scarytom/collider/model/Ball.java
@@ -3,7 +3,7 @@ package com.scarytom.collider.model;
 
 public final class Ball {
 
-    public static final Ball BLACK  = Ball.coloured("#393B36");
+    public static final Ball BLACK  = Ball.coloured("#191B16");
     public static final Ball YELLOW = Ball.coloured("#FDC62B");
     public static final Ball RED    = Ball.coloured("#FF3D46");
     public static final Ball BLUE   = Ball.coloured("#41B8FA");
