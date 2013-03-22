0137dd4d4e5197a18f50bca5fecc03563ae6f7cb
scarytom <t.denley@cantab.net>
2013-03-22 22:52:11 +0000

    basic UI to allow user testing through java desktop

--- src/main/java/com/scarytom/collider/CroquetMaster.java
+++ src/main/java/com/scarytom/collider/CroquetMaster.java
@@ -1,16 +1,22 @@
 package com.scarytom.collider;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingUtilities;
 
 import com.scarytom.collider.model.Ball;
+import com.scarytom.collider.model.BallInPlay;
 import com.scarytom.collider.model.Game;
 import com.scarytom.collider.model.Pitch;
 import com.scarytom.collider.model.Strike;
@@ -22,67 +28,79 @@ import com.scarytom.collider.renderer.TransitionProjector;
 
 public final class CroquetMaster {
 
-    public static void main(String[] args) throws Exception {
-        final Game game = new Game();
+    private final Game game = new Game();
+    private final StrokeEnactor enactor = new StrokeEnactor(game.pitch);
+    private final List<TransitionProjector> projectors = new ArrayList<TransitionProjector>();
 
-        final StrokeEnactor enactor = new StrokeEnactor(game.pitch);
+    private List<BallInPlay> ballPositions = game.ballPositions;
 
+    public CroquetMaster() {
         SwingUtilities.invokeLater(new Runnable() {
             @Override public void run() {
                 final PitchView view = new PitchView(new Pitch());
-                final TransitionProjector projector = new TransitionProjector(view);
+                projectors.add(new TransitionProjector(view));
                 
                 final JFrame frame = new JFrame("Testaroonie");
                 frame.setLayout(new BorderLayout());
                 frame.add(view, "Center");
                 frame.pack();
-                
-                view.draw(game.ballPositions);
-
-                SwingUtilities.invokeLater(new Runnable() {
-                    @Override public void run() {
-                        Stroke s = showOptionPane();
-                        if (null != s) {
-                            final Transition t = enactor.makeStroke(game.ballPositions, s);
-                            projector.project(t);
-                        }
-                    }
-                });
-
+                view.draw(ballPositions);
                 frame.setVisible(true);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                
+                showControlsFrame();
             }
-
         });
     }
     
-    private static Stroke showOptionPane() {
-        Object[] possibilities = {Ball.BLUE, Ball.BLACK, Ball.YELLOW, Ball.RED}; 
-        JSpinner angleSpinner = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 360.0, 1.0));
-        JSpinner powerSpinner = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 20.0, 1.0));
-        JPanel panel = new JPanel();
+    public static void main(String[] args) throws Exception {
+        new CroquetMaster();
+    }
+    
+    private void showControlsFrame() {
+        final JFrame frame = new JFrame();
+        frame.setLayout(new BorderLayout());
+
+        final Ball[] possibilities = {Ball.BLUE, Ball.BLACK, Ball.YELLOW, Ball.RED}; 
+        final JSpinner angleSpinner = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 360.0, 1.0));
+        final JSpinner powerSpinner = new JSpinner(new SpinnerNumberModel(0.0, 0.0, 20.0, 1.0));
+        final JComboBox<Ball> ballChooser = new JComboBox<>(possibilities);
+        final JButton goButton = new JButton("Go!");
+        
+        final JPanel panel = new JPanel();
         panel.add(new JLabel("angle:"));
         panel.add(angleSpinner);
         panel.add(new JLabel("power:"));
         panel.add(powerSpinner);
         panel.add(new JLabel("ball:"));
-        Ball b = (Ball)JOptionPane.showInputDialog(
-                            null,
-                            panel,
-                            "Customized Dialog",
-                            JOptionPane.PLAIN_MESSAGE,
-                            null,
-                            possibilities,
-                            Ball.BLUE);
-
-        //If a string was returned, say so.
-        if (b != null) {
-            Double power = (Double)powerSpinner.getValue();
-            final float angle = (float)Math.toRadians((double)angleSpinner.getValue());
-            return Stroke.standard(b, new Strike(angle, power.floatValue()));
-        }
-        return null;
+        panel.add(ballChooser);
+        panel.add(goButton);
+        
+        goButton.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent e) {
+                goButton.setEnabled(false);
+                final Ball ball = ballChooser.getItemAt(ballChooser.getSelectedIndex());
+                final Double power = (Double)powerSpinner.getValue();
+                final float angle = (float)Math.toRadians((double)angleSpinner.getValue());
+                makeShot(goButton, Stroke.standard(ball, new Strike(angle, power.floatValue())));
+            }
+        });
+        
+        frame.add(panel, "Center");
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    private void makeShot(final JButton goButton, final Stroke stroke) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override public void run() {
+                    final Transition t = enactor.makeStroke(ballPositions, stroke);
+                    for (TransitionProjector projector : projectors) {
+                        projector.project(t);
+                        ballPositions = t.finalPositions();
+                    }
+                    goButton.setEnabled(true);
+            }
+        });
     }
 }
 
