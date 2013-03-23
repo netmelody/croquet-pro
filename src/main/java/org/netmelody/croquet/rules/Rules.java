e85d495658c9ab15e89a647e1526c582a968741d
scarytom <t.denley@cantab.net>
2013-03-23 14:00:38 +0000

    extra stroke for running a hoop

--- src/main/java/org/netmelody/croquet/rules/Rules.java
+++ src/main/java/org/netmelody/croquet/rules/Rules.java
@@ -4,11 +4,18 @@ import java.util.List;
 
 import org.netmelody.croquet.model.Ball;
 import org.netmelody.croquet.model.StrokeEvent;
+import org.netmelody.croquet.model.StrokeEventType;
 import org.netmelody.croquet.model.Turn;
 
 public final class Rules {
 
     public Turn adjudicateStroke(Turn currentTurn, Ball ball, List<StrokeEvent<?>> events) {
+        for (StrokeEvent<?> event : events) {
+            if (event.type == StrokeEventType.RUN_HOOP) {
+                return currentTurn;
+            }
+        }
+        
         return currentTurn.finish();
     }
 
--- src/test/java/org/netmelody/croquet/rules/RulesTest.java
+++ src/test/java/org/netmelody/croquet/rules/RulesTest.java
@@ -2,11 +2,16 @@ package org.netmelody.croquet.rules;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.netmelody.croquet.model.StrokeEvent.runHoop;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 import org.netmelody.croquet.model.Ball;
+import org.netmelody.croquet.model.Hoop;
+import org.netmelody.croquet.model.Position;
 import org.netmelody.croquet.model.StrokeEvent;
 import org.netmelody.croquet.model.Team;
 import org.netmelody.croquet.model.Turn;
@@ -20,9 +25,20 @@ public final class RulesTest {
 
     @Test public void
     eventlessStrokeFinishesTurn() {
-        final Turn updatedTurn = rules.adjudicateStroke(turn, Ball.BLACK, new ArrayList<StrokeEvent<?>>());
+        final Turn updatedTurn = rules.adjudicateStroke(turn, Ball.BLACK, events());
 
         assertThat(updatedTurn.finished(), is(true));
     }
 
+    @Test public void
+    runningAHoopWithThePlayedBallGarnersAnExtraStoke() {
+        final Hoop hoop = new Hoop(Position.at(1, 2));
+        final Turn updatedTurn = rules.adjudicateStroke(turn, Ball.BLACK, events(runHoop(Ball.BLACK, hoop)));
+        
+        assertThat(updatedTurn.finished(), is(false));
+    }
+
+    private static List<StrokeEvent<?>> events(StrokeEvent<?>... events) {
+        return new ArrayList<StrokeEvent<?>>(Arrays.asList(events));
+    }
 }
