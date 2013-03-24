60b320e317556ca7dfb062743267bfab50f189a1
scarytom <t.denley@cantab.net>
2013-03-24 15:54:12 +0000

    render balls

--- android/src/org/netmelody/croquet/android/CourtRenderer.java
+++ android/src/org/netmelody/croquet/android/CourtRenderer.java
@@ -1,31 +1,54 @@
 package org.netmelody.croquet.android;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import org.netmelody.croquet.model.BallInPlay;
+import org.netmelody.croquet.model.Hoop;
+import org.netmelody.croquet.model.Pitch;
+import org.netmelody.croquet.physics.Transition;
+
 import android.opengl.GLES20;
-import android.opengl.Matrix;
 import android.opengl.GLSurfaceView.Renderer;
+import android.opengl.Matrix;
 
 public final class CourtRenderer implements Renderer {
 
+	private final Pitch court;
+	
 	private final float[] projectionMatrix = new float[16];
 	private final float[] viewMatrix = new float[16];
 	
 	private final float[] transformationMatrix = new float[16];
-	private Circle[] content = new Circle[]{};
+	private final List<Circle> fixedFeatures = new ArrayList<Circle>();
+
+	private List<BallInPlay> ballPositions = new ArrayList<BallInPlay>();
 	
-	@Override
-	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		
-		content = new Circle[] {
-			new Circle(0.0f, 0.0f, 0.03f),
-			new Circle(0.4f, 0.0f, 0.03f),
-			new Circle(-0.4f, 0.0f, 0.03f)
-		};
+	public CourtRenderer(Pitch court) {
+		this.court = court;
 	}
 
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		GLES20.glClearColor(0.0f, 0.2f, 0.0f, 1.0f);
+		
+		for (Iterator<Hoop> iterator = court.hoops.iterator(); iterator.hasNext();) {
+			final Hoop hoop = iterator.next();
+	    	fixedFeatures.add(scaledCircle(hoop.leg1Position.x, hoop.leg1Position.y, hoop.legRadius));
+	    	fixedFeatures.add(scaledCircle(hoop.leg2Position.x, hoop.leg2Position.y, hoop.legRadius));
+		}
+		fixedFeatures.add(scaledCircle(court.peg.position.x, court.peg.position.y, court.peg.radius));
+	}
+
+	private final Circle scaledCircle(float x, float y, float radius) {
+		float scale = 30.0f;
+		return new Circle((x - court.peg.position.x) / scale, (y - court.peg.position.y) / scale, radius / scale);
+	}
+	
 	@Override
 	public void onSurfaceChanged(GL10 gl, int width, int height) {
 		GLES20.glViewport(0, 0, width, height);
@@ -36,12 +59,29 @@ public final class CourtRenderer implements Renderer {
 	@Override
 	public void onDrawFrame(GL10 gl) {
 		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-		Matrix.setLookAtM(viewMatrix, 0, 0, 0, -3, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+		Matrix.setLookAtM(viewMatrix, 0,
+				          0, 0, -3,
+				          0, 0, 0f,
+				          0.0f, -1.0f, 0.0f);
+		
 		Matrix.multiplyMM(transformationMatrix, 0, projectionMatrix, 0, viewMatrix, 0);
 		
-		for (int i = 0; i < content.length; i++) {
-			content[i].draw(transformationMatrix);
+		for (Iterator<Circle> iterator = fixedFeatures.iterator(); iterator.hasNext();) {
+			iterator.next().draw(transformationMatrix);
 		}
+		
+		for (Iterator<BallInPlay> iterator = ballPositions.iterator(); iterator.hasNext();) {
+			final BallInPlay ballInPlay = iterator.next();
+			scaledCircle(ballInPlay.position.x, ballInPlay.position.y, ballInPlay.ball.radius).draw(transformationMatrix);
+		}
+	}
+
+	public void setBallPositions(List<BallInPlay> ballPositions) {
+		this.ballPositions = ballPositions;
+	}
+
+	public void playStroke(Transition transition) {
+		this.ballPositions = transition.finalPositions();
 	}
 
 }
--- android/src/org/netmelody/croquet/android/CourtView.java
+++ android/src/org/netmelody/croquet/android/CourtView.java
@@ -1,5 +1,16 @@
 package org.netmelody.croquet.android;
 
+import java.util.List;
+
+import org.netmelody.croquet.model.Ball;
+import org.netmelody.croquet.model.BallInPlay;
+import org.netmelody.croquet.model.Game;
+import org.netmelody.croquet.model.Strike;
+import org.netmelody.croquet.model.Stroke;
+import org.netmelody.croquet.model.Team;
+import org.netmelody.croquet.physics.StrokeEnactor;
+import org.netmelody.croquet.physics.Transition;
+
 import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.view.MotionEvent;
@@ -8,17 +19,27 @@ public final class CourtView extends GLSurfaceView {
 
 	private final CourtRenderer renderer;
 
+    private final Game game = new Game(new Team("Team 1"), new Team("Team2"));
+    private final StrokeEnactor enactor = new StrokeEnactor(game.pitch);
+    
+    private List<BallInPlay> ballPositions = game.ballPositions;
+	
 	public CourtView(Context context) {
 		super(context);
 		setEGLContextClientVersion(2);
-		renderer = new CourtRenderer();
+		renderer = new CourtRenderer(game.pitch);
         setRenderer(renderer);
         
+        renderer.setBallPositions(game.ballPositions);
         setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
 	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		return super.onTouchEvent(event);
+		final Transition transition = enactor.makeStroke(ballPositions, Stroke.standard(Ball.BLACK, new Strike(0.05f, 2f)));
+		renderer.playStroke(transition);
+		ballPositions = transition.finalPositions();
+		requestRender();
+		return true;
 	}
 }
